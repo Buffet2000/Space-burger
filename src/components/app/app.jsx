195e7c8fd8d9f3624db69fc520f9dd586c7dccc1
Buffet2000 <103905518+Buffet2000@users.noreply.github.com>
2023-03-03 14:46:48 +0200

    project use data from api

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,13 +4,41 @@ import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import styles from "./app.module.css";
 
+const apiUrl = "https://norma.nomoreparties.space/api/ingredients";
+
 export default function App() {
+  const [state, setState] = React.useState({
+    isLoading: true,
+    hasError: false,
+    data: [],
+  });
+
+  const getIngredients = () => {
+    setState({ ...state, hasError: false, isLoading: true });
+    fetch(apiUrl)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(console.log(`Произошла ошибка: ${res.status}`));
+      })
+      .then(({ data }) => setState({ ...state, data, isLoading: false }))
+      .catch((e) => {
+        setState({ ...state, hasError: true, isLoading: false });
+      });
+  };
+
+  React.useEffect(() => getIngredients(), []);
+
+  const { data, isLoading, hasError } = state;
   return (
     <>
       <AppHeader />
       <div className={styles.page}>
-        <BurgerIngredients />
-        <BurgerConstructor />
+        {isLoading && 'Загрузка...'}
+        {hasError && 'Произошла ошибка'}
+        {!isLoading && !hasError && <BurgerIngredients data={data} />}
+        {!isLoading && !hasError&& <BurgerConstructor data={data} />}
       </div>
     </>
   );
