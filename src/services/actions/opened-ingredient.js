df12e1ecd834f4ca88091e89aecbd31dd71d866e
Buffet2000 <103905518+Buffet2000@users.noreply.github.com>
2023-06-20 16:04:24 +0300

    actions in TypeScript

--- src/services/actions/opened-ingredient.js
+++
@@ -1,7 +0,0 @@
-import { ADD_INGREDIENT_INFO, DELETE_INGREDIENT_INFO } from "../constants/opened-ingredient"; 
-
-export const addIngredientInfo = (payload) => ({
-  type: ADD_INGREDIENT_INFO,
-  payload,
-});
-export const deleteIngredientInfo = () => ({ type: DELETE_INGREDIENT_INFO });
