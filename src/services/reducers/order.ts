8ebfacbc70e23a3d282b9026e98f8457b196e083
Buffet2000 <103905518+Buffet2000@users.noreply.github.com>
2023-06-22 17:06:21 +0300

    smalll fixes and refactoring

--- src/services/reducers/order.ts
+++ src/services/reducers/order.ts
@@ -24,28 +24,20 @@ export const orderReducer = (state = initialState, action: OrderActions) => {
   switch (action.type) {
     case GET_ORDER_NUMBER_REQUEST:
       return { ...state, orderNumberRequest: true };
+
     case GET_ORDER_NUMBER_SUCCESS:
-      return {
-        ...state,
-        orderNumber: action.payload,
-        orderNumberRequest: false,
-        orderNumberFailed: false,
-        isLoaded: true,
-      };
+      return { ...state, orderNumber: action.payload, orderNumberRequest: false, orderNumberFailed: false, isLoaded: true };
+
     case GET_ORDER_NUMBER_FAILED:
-      return { ...state, orderNumberRequest: false, orderNumberFailed: true };
+      return { ...state, orderNumberRequest: false, orderNumberFailed: true, };
+
     case ADD_ORDER:
       return { ...state, orderItems: action.payload };
+
     case DELETE_ORDER:
-      return {
-        ...state,
-        orderItems: [],
-        orderNumber: null,
-        orderNumberRequest: false,
-        orderNumberFailed: false,
-        isLoaded: false,
-      };
+      return { ...state, orderItems: [], orderNumber: null, orderNumberRequest: false, orderNumberFailed: false, isLoaded: false };
+
     default:
       return state;
   }
-};
+}
