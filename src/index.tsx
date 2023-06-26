1d5480e60b88d083490f9a1df6dffb109912dc1f
Buffet2000 <103905518+Buffet2000@users.noreply.github.com>
2023-06-26 15:50:02 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from "./components/app/app";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "./services/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
